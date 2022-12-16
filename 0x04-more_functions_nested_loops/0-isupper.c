3f2d9bb4988f51f2a7e4ce58fb4c3fe7a01acb07
rlabbiz <redalabbiz@gmail.com>
2022-12-16 04:04:05 -0800

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * function - check the c if uppercase or not
+ * if yes return 1
+ * if false return 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
